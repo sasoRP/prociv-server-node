edb081d11eddda5c76b774cc9c56b5226b6f83dd
Luís Peixoto <lmpeixoto@gmail.com>
2020-07-14 12:09:54 +0100

    removed unused imports and prototyped csv download

--- sandbox.js
+++ sandbox.js
@@ -1,62 +1,102 @@
-const BASE_URL = 'http://www.prociv.pt/en-US/Pages/export.aspx?';
+const Fs = require("fs");
+const Path = require("path");
+const Axios = require("axios");
 
-let query_url = `ex=1&l=0&d=&c=&f=&t=0&n=0&e=1`;
-let query_url2 = `http://www.prociv.pt/pt-PT/Paginas/export.aspx?ex=1&l=1&d=&n=&2020.06.01=&2020.06.07=&e=1`;
+const BASE_URL = "http://www.prociv.pt/pt-PT/Paginas/export.aspx?";
+
+const SAVE_FOLDER = Path.resolve(__dirname, "data");
+
+// Parâmetro	Descrição
+// ex	boolean, sempre verdadeiro para exportação
+// l	boolean, se verdadeiro é obrigatório colocar um intervalo de datas, caso contrário exporta o dia corrente.
+// d	distrito (ver tabela, deixar sem Id para todos os distritos)
+// n	código da natureza da ocorrência (ver tabela, deixar sem Id para todas as naturezas de ocorrência)
+// s	data início (ex: 2017.06.01)
+// f	data final (ex: 2017.06.30)
+// e	tipo de exportação (XLSX: 0, CSV: 1, KMZ: 2)
+
+let queryObjectInstantData = {
+  ex: 1,
+  l: 0,
+  d: "",
+  n: 0,
+  e: 1,
+};
+
+let queryObjectTimeHistoryData = {
+  ex: 1,
+  l: 0,
+  d: "",
+  n: "",
+  s: "2016.01.01",
+  f: "2016.01.01",
+  e: 1,
+};
 
 const DISTRITOS = {
-    1: 'Aveiro',
-    2: 'Beja',
-    3: 'Braga',
-    4: 'Bragança',
-    5: 'Castelo Branco',
-    6: 'Coimbra',
-    8: 'Évora',
-    9: 'Faro',
-    10: 'Guarda',
-    11: 'Leiria',
-    12: 'Lisboa',
-    13: 'Portalegre',
-    14: 'Porto',
-    15: 'Santarém',
-    16: 'Setúbal',
-    17: 'Viana do Castelo',
-    18: 'Vila Real',
-    19: 'Viseu'
+  1: "Aveiro",
+  2: "Beja",
+  3: "Braga",
+  4: "Bragança",
+  5: "Castelo Branco",
+  6: "Coimbra",
+  8: "Évora",
+  9: "Faro",
+  10: "Guarda",
+  11: "Leiria",
+  12: "Lisboa",
+  13: "Portalegre",
+  14: "Porto",
+  15: "Santarém",
+  16: "Setúbal",
+  17: "Viana do Castelo",
+  18: "Vila Real",
+  19: "Viseu",
 };
 
 const COLUNAS_CSV = [
-    'Numero',
-    'DataOcorrencia',
-    'DataFechoOperacional',
-    'Natureza',
-    'EstadoOcorrencia',
-    'Distrito',
-    'Concelho',
-    'Freguesia',
-    'Localidade',
-    'Latitude',
-    'Longitude',
-    'NumeroMeiosTerrestresEnvolvidos',
-    'NumeroOperacionaisTerrestresEnvolvidos',
-    'NumeroMeiosAereosEnvolvidos',
-    'NumeroOperacionaisAereosEnvolvidos'
+  "Numero",
+  "DataOcorrencia",
+  "DataFechoOperacional",
+  "Natureza",
+  "EstadoOcorrencia",
+  "Distrito",
+  "Concelho",
+  "Freguesia",
+  "Localidade",
+  "Latitude",
+  "Longitude",
+  "NumeroMeiosTerrestresEnvolvidos",
+  "NumeroOperacionaisTerrestresEnvolvidos",
+  "NumeroMeiosAereosEnvolvidos",
+  "NumeroOperacionaisAereosEnvolvidos",
 ];
 
-let rows;
+let queryInstantData = `ex=${queryObjectInstantData.ex}&l=${queryObjectInstantData.l}&d=${queryObjectInstantData.d}&c=&f=&t=0&n=${queryObjectInstantData.n}&e=${queryObjectInstantData.e}`;
+let queryTimeHistoryData = `ex=${queryObjectTimeHistoryData.ex}&l=${queryObjectTimeHistoryData.l}&d=${queryObjectTimeHistoryData.d}&n=&${queryObjectTimeHistoryData.s}=&${queryObjectTimeHistoryData.f}=&e=${queryObjectTimeHistoryData.e}`;
 
-const axios = require('axios');
-const neatCsv = require('neat-csv');
+let dataUrl = BASE_URL + queryInstantData;
 
-let csvUrl = BASE_URL + query_url;
-const getData = async (url) => {
-    try {
-        const response = await axios.get(url);
-        const data = response.data;
-        rows = await neatCsv(data);
-    } catch (error) {
-        console.log(error);
-    }
-    console.log(rows);
-};
+console.log(dataUrl);
 
-getData(csvUrl);
+async function downloadCSV(url, path) {
+  if (!Fs.existsSync(path)) {
+    Fs.mkdirSync(path);
+  }
+  const writer = Fs.createWriteStream(Path.resolve(SAVE_FOLDER, "data.csv"));
+
+  const response = await Axios({
+    url,
+    method: "GET",
+    responseType: "stream",
+  });
+
+  response.data.pipe(writer);
+
+  return new Promise((resolve, reject) => {
+    writer.on("finish", resolve);
+    writer.on("error", reject);
+  });
+}
+
+downloadCSV(dataUrl, SAVE_FOLDER);
--- server.js
+++ server.js
@@ -1,18 +1,14 @@
-const express = require('express');
-const path = require('path');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const controllers = require('./controllers');
+const controllers = require("./controllers");
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
-app.use(bodyParser.urlencoded({ extended: false }));
-
-app.get('/', controllers.getIndex);
+app.get("/", controllers.getIndex);
 
 app.listen(port, () =>
-    console.log(`App listening at http://localhost:${port}`)
+  console.log(`App listening at http://localhost:${port}`)
 );
