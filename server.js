1d59430f37ff240fef43e92d83c6332b2b687bd4
Luis Peixoto <lmpeixoto@gmail.com>
2020-07-18 08:33:26 +0100

    separated config .env

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const dotenv = require('dotenv').config();
 
 const controllers = require('./controllers');
 const app = express();
 const port = process.env.PORT || 3000;
+const MONGODB_URI = process.env.MONGODB_URI;
+
+mongoose.connect(MONGODB_URI, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+});
+
+const connection = mongoose.connection;
+
+connection.once('open', function () {
+    console.log('MongoDB database connection established successfully');
+});
 
 app.use(express.json());
 
